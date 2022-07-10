3c0221b52f99e062fd2232001586332d6d888f33
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-07-10 10:19:18 -0400

    debugging

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,5 @@
-const Player = require('../lib/Player.js');
-const Potion = require('../lib/Potion.js');
+const Player = require('../Lib/Player.js');
+const Potion = require('../Lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
 
